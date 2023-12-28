64f728773cc5b1163aaf9a781ac38034da0097a8
LHufton <lukehufton@gmail.com>
2023-12-28 03:43:47 -0600

    improving react on search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,23 @@
+import React, { useState } from 'react'
 import './Search.css'
 
-return (
-  <div className="App">
-    <h2>Whatever the Weather</h2>
-    <form onSubmit={handleCitySearch} className="search-bar">
-      <input
-        className="search-bar-input"
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Enter City"
-      />
-    </form>
-  </div>
-)
+const Search = ({ handleCitySearch }) => {
+  const [inputValue, setInputValue] = useState('')
+
+  return (
+    <div className="App">
+      <h2>Whatever the Weather</h2>
+      <form onSubmit={handleCitySearch} className="search-bar">
+        <input
+          className="search-bar-input"
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter City"
+        />
+      </form>
+    </div>
+  )
+}
+
+export default Search
