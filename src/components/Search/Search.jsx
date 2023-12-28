a08ba999fd2b6c636c445faca9c17ad021c43137
LHufton <lukehufton@gmail.com>
2023-12-28 02:46:41 -0600

    search component fix

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,16 @@
+import './Search.css'
+
+return (
+  <div className="App">
+    <h2>Whatever the Weather</h2>
+    <form onSubmit={handleCitySearch} className="search-bar">
+      <input
+        className="search-bar-input"
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter City"
+      />
+    </form>
+  </div>
+)
